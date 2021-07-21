413298708d757a693c83b96aff0353945f412957
FelipeAlbertoCarvalho <felipecarvalho@alunos.utfpr.edu.br>
2021-07-21 04:41:16 -0300

    adicionado apresentação da notificacao

--- Cliente_Viagens_Carona/src/CliImpl.java
+++ Cliente_Viagens_Carona/src/CliImpl.java
@@ -10,13 +10,30 @@ public class CliImpl extends UnicastRemoteObject implements InterfaceCli{
     }
 
     public void notificar(String nome, String contato, int tipo) throws RemoteException{
-        String usuario;
-        if(tipo == 0)
-            usuario = "passageiro";
-        else
-            usuario = "motorista";
-        System.out.println("NOTIFICACAO\nSeguem os dados do " + usuario + ":");
-        System.out.println("Nome: " + nome + "\nContato: " + contato);
+        String[] usuario = new String[2];
         
+        
+        if(tipo == 0) {
+        	usuario[0] = "PASSAGEIRO";
+            usuario[1] = "MOTORISTA";
+        }
+        else {
+        	usuario[0] = "MOTORISTA";
+            usuario[1] = "PASSAGEIRO";
+        }
+        
+        System.out.println("#################################################");
+        System.out.println();
+        System.out.println("\tNOTIFICACAO PARA O " + usuario[1]);
+        System.out.println();
+        System.out.println("OLA " + usuario[1] + " SEGUEM OS DADOS DO " + usuario[0] + ":");
+        System.out.println();
+        System.out.println("\tNOME:\t\t " + nome );
+        System.out.println();
+        System.out.println("\tCONTATO:\t " + contato);
+        System.out.println();
+        System.out.println("\t(: tenha uma ótima viagem :)");
+        System.out.println("#################################################");
+
     }
 }
